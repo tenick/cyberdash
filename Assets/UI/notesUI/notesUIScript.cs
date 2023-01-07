93cd465df65816bcc27d1747908b3743205c546d
tenick <usareskenneth22@gmail.com>
2023-01-07 20:14:14 +0800

    updated notes UI, updated dialogs

--- Assets/UI/notesUI/notesUIScript.cs
+++ Assets/UI/notesUI/notesUIScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class notesUIScript : MonoBehaviour
 {
@@ -10,16 +11,33 @@ public class notesUIScript : MonoBehaviour
     public GameObject NotesPanel;
     public characterAnimScript CharacterScript;
 
+    public Image NotePageImg;
+    public List<Sprite> NotePages;
+    public int pageIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NotePageImg.sprite = NotePages[pageIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void NextNote()
+    {
+        pageIndex = Math.Clamp(pageIndex + 1, 0, NotePages.Count);
+        NotePageImg.sprite = NotePages[pageIndex];
+
+    }
+
+    public void PrevNote()
+    {
+        pageIndex = Math.Clamp(pageIndex - 1, 0, NotePages.Count);
+        NotePageImg.sprite = NotePages[pageIndex];
     }
 
     public void OpenNote()
